c9028473f718dd04e0d287d2f3949ae607fcdbdd
Lin Jen-Shin <godfat@godfat.org>
2014-07-09 15:57:33 -0700

    put stats in the bottom

--- lib/pork.rb
+++ lib/pork.rb
@@ -19,41 +19,6 @@ module Pork
     end
   end
 
-  class Stats < Struct.new(:tests, :assertions, :skips, :failures, :errors)
-    def initialize
-      @mutex = Mutex.new
-      super(0, 0, 0, [], [])
-    end
-    def assertions= num; @mutex.synchronize{ super                    }; end
-    def tests=      num; @mutex.synchronize{ super                    }; end
-    def skips=      num; @mutex.synchronize{ super        ; print('s')}; end
-    def add_failure *e ; @mutex.synchronize{ failures << e; print('F')}; end
-    def add_error   *e ; @mutex.synchronize{ errors   << e; print('E')}; end
-    def numbers
-      [tests, assertions, failures.size, errors.size, skips]
-    end
-    def start
-      @start ||= Time.now
-    end
-    def report
-      puts
-      puts (failures + errors).map{ |(e, m)|
-        "\n#{m}\n#{e.class}: #{e.message}\n  #{backtrace(e)}"
-      }
-      printf("\nFinished in %f seconds.\n", Time.now - @start)
-      printf("%d tests, %d assertions, %d failures, %d errors, %d skips\n",
-             *numbers)
-    end
-    private
-    def backtrace e
-      if $VERBOSE
-        e.backtrace
-      else
-        e.backtrace.reject{ |line| line =~ %r{/pork\.rb:\d+} }
-      end.join("\n  ")
-    end
-  end
-
   class Executor < Struct.new(:name)
     extend Pork::API
     def self.execute caller, desc, &suite
@@ -194,6 +159,41 @@ module Pork
       else
         ''
       end
+    end
+  end
+
+  class Stats < Struct.new(:tests, :assertions, :skips, :failures, :errors)
+    def initialize
+      @mutex = Mutex.new
+      super(0, 0, 0, [], [])
+    end
+    def assertions= num; @mutex.synchronize{ super                    }; end
+    def tests=      num; @mutex.synchronize{ super                    }; end
+    def skips=      num; @mutex.synchronize{ super        ; print('s')}; end
+    def add_failure *e ; @mutex.synchronize{ failures << e; print('F')}; end
+    def add_error   *e ; @mutex.synchronize{ errors   << e; print('E')}; end
+    def numbers
+      [tests, assertions, failures.size, errors.size, skips]
+    end
+    def start
+      @start ||= Time.now
+    end
+    def report
+      puts
+      puts (failures + errors).map{ |(e, m)|
+        "\n#{m}\n#{e.class}: #{e.message}\n  #{backtrace(e)}"
+      }
+      printf("\nFinished in %f seconds.\n", Time.now - @start)
+      printf("%d tests, %d assertions, %d failures, %d errors, %d skips\n",
+             *numbers)
+    end
+    private
+    def backtrace e
+      if $VERBOSE
+        e.backtrace
+      else
+        e.backtrace.reject{ |line| line =~ %r{/pork\.rb:\d+} }
+      end.join("\n  ")
     end
   end
 end
