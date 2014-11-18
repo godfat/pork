b3573f52dc4d0b41ef4d0956e12a3ace0650235f
Lin Jen-Shin <godfat@godfat.org>
2014-11-19 05:05:35 +0800

    forgot to commit isolation test

--- test/test_isolate.rb
+++ test/test_isolate.rb
@@ -4,12 +4,11 @@ require 'pork/isolate'
 
 describe Pork::Isolate do
   would '#isolate' do
-    skip
-    20.times do
+    50.times do
       name = Pork::Executor.all_tests.keys.sample
       next ok if name.start_with?('Pork::Isolate #isolate #')
-      executor = Pork::Executor.isolate(name).execute
-      expect(executor.passed?, executor.stat.inspect).eq true
+      stat = Pork::Executor.isolate(name)
+      expect(stat.passed?, stat.inspect).eq true
     end
   end
 end
