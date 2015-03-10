e32e2e89b5205a8f0bf816cbec157dcf551e4fb5
Lin Jen-Shin <godfat@godfat.org>
2015-03-11 01:26:32 +0800

    aha. that's it

--- lib/pork/mode/parallel.rb
+++ lib/pork/mode/parallel.rb
@@ -10,9 +10,7 @@ module Pork
     def parallel stat=Stat.new, paths=all_tests.values.flatten(1)
       paths.shuffle.each_slice(cores).map do |paths_slice|
         Thread.new do
-          s = Stat.new
-          paths_slice.each{ |p| execute(:shuffled, s, [p]) }
-          s
+          execute(:shuffled, Stat.new, paths_slice)
         end
       end.map(&:value).inject(stat, &:merge)
     end
