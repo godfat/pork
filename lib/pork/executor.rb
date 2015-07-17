e1f96be3dfaa80b995551e315dc7bcf0d507a95b
Lin Jen-Shin <godfat@godfat.org>
2015-07-17 23:15:20 +0800

    remove confusing method

--- lib/pork/executor.rb
+++ lib/pork/executor.rb
@@ -4,6 +4,10 @@ require 'pork/context'
 
 module Pork
   class Executor < Struct.new(:pork_stat, :pork_description)
+    # we don't want this method from Struct.new, it's confusing when
+    # pork/isolate was not loaded. (i.e. isolate would override it anyway)
+    singleton_class.superclass.send(:remove_method, :[])
+
     extend Imp
     include Context
     init
