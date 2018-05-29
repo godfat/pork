4e0f470e342a8c8ed8391fe70d8e55f21d645db2
Lin Jen-Shin <godfat@godfat.org>
2018-05-29 16:26:40 +0800

    Add ShowSource::FrozenStringFix for method_source
    And enable --enable-frozen-string-literal on CI

--- lib/pork/extra/show_source.rb
+++ lib/pork/extra/show_source.rb
@@ -3,6 +3,25 @@ require 'method_source'
 
 module Pork
   module ShowSource
+    module FrozenStringFix
+      def extract_first_expression(lines, consume=0, &block)
+        code = consume.zero? ? [] : lines.slice!(0..(consume - 1))
+
+        lines.each do |v|
+          code << v
+
+          result = block ? block.call(code.join) : code.join
+
+          if complete_expression?(result)
+            return result
+          end
+        end
+        raise SyntaxError, "unexpected $end"
+      end
+    end
+
+    MethodSource.extend(FrozenStringFix)
+
     def show_source test, err
       source = test.source
       sopath = "#{test.source_location.first}:"
